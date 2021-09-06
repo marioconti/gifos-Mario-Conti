1abf0087d443d649ef9faf87d12c1189b322566d
Mario Conti <marioConti91@gmail.com>
2021-09-06 09:53:08 -0300

    se agregan los react components de la imagen del filtro y de la lupa para hacer testing. No renderiza

--- src/Filtro/Filtro.jsx
+++ src/Filtro/Filtro.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Filtro.css";
-// import { ReactComponent as ImagenHeader } from "../Imagenes/ilustra_header.svg";
+import { ReactComponent as ImagenHeader } from "../Imagenes/ilustra_header.svg";
+import { ReactComponent as ImagenLupa } from "../Imagenes/icon-search.svg";
 export const Filtro = () => {
   return (
     <>
@@ -8,11 +9,13 @@ export const Filtro = () => {
         <p>
           ¡Inspirate y busca los mejores <span>GIFS!</span>{" "}
         </p>
+        <ImagenHeader />
         {/* FIXME:arreglar la imagen grande aquí, pedir ayudar a fausto reportar error. Debe ser porq está como en partes hecha la imagen */}
         <div className="buscador">
           <input className="filtro" type="text" placeholder="Busca gifs" />
-          <button className="btn-buscar" type="button"> FONT
-          {/* FIXME:arreglar aquí también problema con el font que no puedo mostrar */}
+          <button className="btn-buscar" type="button">
+            <ImagenLupa />
+            {/* FIXME:arreglar aquí también problema con el font que no puedo mostrar */}
           </button>
         </div>
       </div>
